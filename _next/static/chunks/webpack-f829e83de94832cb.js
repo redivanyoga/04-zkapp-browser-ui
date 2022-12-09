fe956cfd6a84a9384b62c52eb64826b442634ca2
redivanyoga <yohanesredivan7@gmail.com>
2022-12-09 10:01:02 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-f829e83de94832cb.js
+++ _next/static/chunks/webpack-f829e83de94832cb.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "5f0b45bc7b2f3778" + ".css";
+/******/ 			return "static/css/" + "e40e8f5b3aff917e" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
